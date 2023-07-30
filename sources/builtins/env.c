06309e644e23bc2d1df1161441676f528597891b
Clement BERNAZEAU <clem.bernazeau@orange.fr>
2023-07-30 17:10:21 +0200

    je sais plus quoi dire

--- sources/builtins/env.c
+++ sources/builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amontign <amontign@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cbernaze <cbernaze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 11:19:50 by amontign          #+#    #+#             */
-/*   Updated: 2023/07/27 10:59:30 by amontign         ###   ########.fr       */
+/*   Updated: 2023/07/30 15:54:14 by cbernaze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	builtin_env(char **args, t_data *env, int fd)
 {
 	if (args[1])
 	{
-		ft_putstr_fd("minishell: env: Bad usage", 2);
+		ft_putstr_fd("minishell: env: Bad usage\n", 2);
 		ft_putstr_fd("env: usage: env\n", 2);
 		return (1);
 	}
